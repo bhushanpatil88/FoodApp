2aecaf4c19fde0127dbb1b2d9a656cb0c17a1073
bhushanpatil88 <bhus.patil88@gmail.com>
2024-10-06 11:31:34 +0530

    Updates backend

---
+++ FoodApp/src/main/java/com/example/FoodApp/controller/RestaurantController.java
@@ -0,0 +1,64 @@
+package com.example.FoodApp.controller;
+
+import com.example.FoodApp.Entity.Restaurant;
+import com.example.FoodApp.Entity.User;
+import com.example.FoodApp.Request.CreateRestaurantRequest;
+import com.example.FoodApp.Service.RestaurantService;
+import com.example.FoodApp.Service.UserService;
+import com.example.FoodApp.dto.RestaurantDto;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/restaurants")
+public class RestaurantController {
+    @Autowired
+    private RestaurantService restaurantService;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Restaurant>> searchRestaurant(@RequestHeader("Authorization") String jwt,
+                                                             @RequestParam String keyword) throws Exception {
+
+        User user = userService.findUserByJwtToken(jwt);
+
+        List<Restaurant> restaurants = restaurantService.searchRestaurant(keyword);
+        return new ResponseEntity<>(restaurants, HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Restaurant>> getAllRestaurants(@RequestHeader("Authorization") String jwt) throws Exception {
+        User user = userService.findUserByJwtToken(jwt);
+        List<Restaurant> restaurants = restaurantService.getAllRestaurant();
+        return new ResponseEntity<>(restaurants, HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Restaurant> findRestaurantById(@RequestHeader("Authorization") String jwt,
+                                                         @PathVariable ObjectId id) throws Exception {
+
+        User user = userService.findUserByJwtToken(jwt);
+
+        Restaurant restaurant = restaurantService.findRestaurantById(id);
+        return new ResponseEntity<>(restaurant, HttpStatus.OK);
+    }
+
+    @PutMapping("{id}/add-to-favorites")
+    public ResponseEntity<RestaurantDto> addToFavorites(@RequestHeader("Authorization") String jwt,
+                                                         @PathVariable ObjectId id) throws Exception {
+
+        User user = userService.findUserByJwtToken(jwt);
+        RestaurantDto restaurantDto = restaurantService.addToFavorites(id, user);
+        return new ResponseEntity<>(restaurantDto, HttpStatus.OK);
+    }
+
+
+
+}
